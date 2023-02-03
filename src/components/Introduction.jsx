deefdb011a39cef79228e416fab90ead3c831e32
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-02-02 21:09:53 -0300

    add javascript window

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 
 const Introduction = () => {
   useEffect(() => {
     const saluteTextContainer = document.querySelector(".saluteTextContainer");
+    const jobTitleContainers = document.querySelectorAll(".jobTitleContainer");
+
     const authorName = document.querySelector(".name");
     const jobTitles = document.querySelectorAll(".jobTitle");
 
@@ -13,6 +15,8 @@ const Introduction = () => {
       authorName.style.transform = `translateX(calc(-100vh + ${offsetY}px))`;
       jobTitles[0].style.transform = `translateX(calc(250vh - ${offsetY}px))`;
       jobTitles[1].style.transform = `translateX(calc(-280vh + ${offsetY}px))`;
+      jobTitleContainers[0].style.backgroundPositionY = `${offsetY * 1.5}px`;
+      jobTitleContainers[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
     });
   }, []);
 
